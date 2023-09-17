653d5921a50e6fda4bc7a7093b5bc01b0ddecd56
Rob Siegwart <rob@robsiegwart.com>
2023-09-17 00:41:01 -0500

    Add openpyxl to setup requires

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     version=0.1,
     packages=['pyBOM'],
     install_requires=[
-        'pandas>=1.0.5',
-        'anytree>=2.8.0'
-    ],
-    python_requires='>=3.5'
+        'pandas',
+        'anytree',
+        'openpyxl'
+    ]
 )
